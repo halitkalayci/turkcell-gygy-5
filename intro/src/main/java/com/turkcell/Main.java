1a20a308cce02e118cafeed12ea7441f64e1568c
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2026-04-01 19:51:29 +0300

    değişkenler giriş

--- intro/src/main/java/com/turkcell/Main.java
+++ intro/src/main/java/com/turkcell/Main.java
@@ -1,8 +1,25 @@
 package com.turkcell;
 
 // Entrypoint
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Merhaba Turkcell");
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Merhaba Turkcell, Java'ya hoş geldiniz!");
+
+        // Programlama konseptleri
+
+        // Scope Kavramı => {} kapsama alanı
+
+        // Değişkenler (Variables)
+        // Kodun akışında değer tutan isimli veriler.
+
+        System.out.println(10);
+        int X = 15; // Değişken tanımlandı. X ismine bir değer atandı.
+        // Tanımlandıktan itibaren değişebilir, erişilebilir.
+        System.out.println(X);
+        X=20;
+        System.out.println(X);
     }
-}
+    
+} // Main classının kapsama alanı (sınır)
